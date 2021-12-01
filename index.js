6d4bd07aacf3a65c2e8eee1f39d8da9bb70a10c1
Tomas Macchi <muma.sanmartin2011@gmail.com>
2021-12-01 18:50:07 -0300

    [add] accordion adjusting responsiveness

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 function btnStyling1(){
+    let width = window.innerWidth;
 
+    // console.log("testing width", width);
 
     const btn1 = document.getElementById("toggle1")
     const btn2 = document.getElementById("toggle2")
@@ -8,35 +10,73 @@ function btnStyling1(){
     const table1 = document.getElementById("table1")
     const table2 = document.getElementById("table2")
 
-    if(btn1.className == "selected"){
-       return null
-    }
-    else{
+    const tableContainer1 = document.getElementById("table_container1")
+    const tableContainer2 = document.getElementById("table_container2")
+
+    // console.log(tableContainer1.className,btn1.className);
+
+
+    if(btn1.className != "selected" && width > 451){
         btn1.className = "selected"
         btn2.className = ""
         image.className = "background--image2"
         table1.hidden = false
         table2.hidden = true
-
+    }
+    else if(btn1.className != "selected"){
+        btn1.className = "selected"
+        btn2.className = ""
+        tableContainer1.className = "show--accordion--function1"
+        tableContainer2.className = "hide--accordion"
+        // table1.hidden = false
+        // table2.hidden = true
+    }
+    else if(tableContainer1.className == "show--accordion--function1" && btn1.className == "selected"){
+        tableContainer1.className = "hide--accordion"
+    }
+    else{
+        return null
     }
 }
 
 function btnStyling2(){
+
+    let width = window.innerWidth;
+
+
     const btn1 = document.getElementById("toggle1")
     const btn2 = document.getElementById("toggle2")
     const image = document.getElementById("section_three")
     const table1 = document.getElementById("table1")
-    const table2 = document.getElementById("table2")
+    const table2 = document.getElementById("table2") 
 
-    if(btn2.className == "selected"){
-       return null
-    }
-    else{
+
+    const tableContainer1 = document.getElementById("table_container1")
+    const tableContainer2 = document.getElementById("table_container2")
+
+
+    if(btn2.className != "selected" && width > 451){
         btn2.className = "selected"
         btn1.className = ""
         image.className = "background--image1"
         table1.hidden = true
         table2.hidden = false
+    }
+    else if(btn2.className != "selected"){
+        btn2.className = "selected"
+        btn1.className = ""
+
+        tableContainer2.className = "show--accordion--function2"
+        tableContainer1.className = "hide--accordion"
+
+        // table1.hidden = true
+        // table2.hidden = false
+    }
+    else if(tableContainer2.className == "show--accordion--function2" && btn2.className == "selected"){
+        tableContainer2.className = "hide--accordion"
+    }
+    else{
+        return null
     }
 }
 
@@ -82,12 +122,46 @@ function goBackImg(){
 // button function
 window.onload = function(){
 	console.log("window.onload");
+    resize()
 	window.addEventListener('resize', resize)
 
 };
 
 function resize(e){
+    // tables containers
+    const tableContainer1 = document.getElementById("table_container1")
+    const tableContainer2 = document.getElementById("table_container2")
+
+    // tables
+    const table1 = document.getElementById("table1")
+    const table2 = document.getElementById("table2") 
+
+    // section
+    const section_three = document.getElementById("section_three")
+
     let heigth = window.innerHeight;
     let width = window.innerWidth;
-    console.log(heigth, width);
+
+    if( width < 450){
+        table1.hidden = false
+        table2.hidden = false
+
+        tableContainer1.className = "hide--accordion"
+        tableContainer2.className = "hide--accordion"
+        
+        tableContainer1.appendChild(table1)
+        tableContainer2.appendChild(table2)
+        section_three.className = ""
+
+    }
+    else{
+        section_three.appendChild(table1)
+        section_three.appendChild(table2)
+
+        section_three.className = "background--image1"
+
+        tableContainer1.className = "hide--accordion"
+        tableContainer2.className = "hide--accordion"
+    }
+
 }
